00f8b19bd43ffa63e274ea42c657bd9e2ebe9b11
Mohammed Ashour <m.aly.ashour@gmail.com>
2020-07-19 21:54:20 +0200

    adding data analysis on the cleaned data and more data cleaning :fire:

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,3 @@
-
-
 import pandas as pd
 
 jobs = pd.read_csv("./jobs_clean.csv")
@@ -31,4 +29,36 @@ jobs["company_age"] = jobs["Founded"].apply(lambda x : 2020 - int(x) if x != -1 
 jobs["is_headquarters"] = jobs["Location"] == jobs["Headquarters"]
 jobs["is_headquarters"] = jobs["is_headquarters"].apply(lambda x: 1 if x else 0)
 
+jobs["Job Title"] = jobs["Job Title"].apply(lambda x : x.lower())
+# jobs["seniority"] = jobs["title"]
+def seniority(title):
+    if "junior"in title or "jr" in title or "jr" in title or "jun" in title:
+        return "jr"
+    elif "ii" in title or "level 2" in title or "2" in title :
+        return "mid-level"
+    elif "senior" in title or "sr." in title or "sr" in title :
+        return "sr"
+    elif "lead" in title or "principal" in title or "staff" in title:
+        return "lead"
+    else: 
+        return "not mentioned"
+def clean_titles(title):
+
+    if "data scientist" in title or "data science" in title:
+        return "data scientist"
+    elif "software engineer" in title:
+        return "software engineer"
+    elif "ml engineer" in title or "machine learning" in title or "deep learning" in title:
+        return "ml engineer"
+    elif "computer vision" in title:
+        return "ml engineer"
+    elif "data engineer" in title or "data management":
+        return "data engineer"
+    else:
+        return title
+
+jobs["seniority"] = jobs["Job Title"].apply(seniority)
+jobs["Job Title"] = jobs["Job Title"].apply(clean_titles)
+    
+jobs["comp_num"] = jobs["Competitors"].apply(lambda x: len(x.split(",")) if x != "-1" else 0)
 jobs.to_csv("./salaries_cleaned.csv")
