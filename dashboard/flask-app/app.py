9f5c4a09724b4657bf902a43723a8aee5d7ae6b7
Mohammed Ashour <m.aly.ashour@gmail.com>
2020-07-23 11:56:32 +0200

    Made the visuals more colorful :fire:

--- dashboard/flask-app/app.py
+++ dashboard/flask-app/app.py
@@ -7,8 +7,9 @@ from bokeh.embed import components
 from bokeh.models import ColumnDataSource, HoverTool, PrintfTickFormatter
 from bokeh.plotting import figure
 from bokeh.transform import factor_cmap
-
+import sys
 from flask import Flask, render_template, request
+from bokeh.palettes import Spectral6
 
 df = pd.read_csv("../data/salaries_analysis_cleaned.csv")
     
@@ -17,12 +18,12 @@ app = Flask(__name__)
  
 
 def plot(x_data, y_data, title):
-    x = list(x_data)[:10]
+    x = [str(i) for i in list(x_data)[:10]]
     y = list(y_data)[:10]
     
     p = figure(x_range=x, plot_height=500, title=title,
            toolbar_location=None)
-    p.vbar(x=x, top=y, width=1)
+    p.vbar(x=x, top=y, width=1, fill_color=factor_cmap('x', palette=Spectral6, factors=x))
 
     p.xgrid.grid_line_color = None
     p.y_range.start = 0
@@ -33,24 +34,36 @@ def plot(x_data, y_data, title):
 @app.route('/', methods=['GET', 'POST'])
 
 def chart():
+    cat_cols = ["Industry", "python", "R","is_headquarters", "seniority", "Job Title"]
+    kwargs = dict()
+    for i in cat_cols:
+        script, div = plot(df[i].value_counts().index\
+                                              , df[i].value_counts(),
+                                              title= i + " Job Posts Count")
+        kwargs[i.replace(" ", "_") + "_script"] = script
+        kwargs[i.replace(" ", "_") + "_div"] = div
+    print(kwargs.keys(),  file=sys.stderr)
 
-    script_industries,  div_industries = plot(df["Industry"].value_counts().index\
-                                              , df["Industry"].value_counts(),
-                                              title="Industry Job Posts Count")
-    script_companies, div_companies = plot(df["Company Name"].value_counts().index\
-                                              , df["Company Name"].value_counts(),\
-                                              title="Company Job Posts Count")
-    script_titles, div_titles = plot(df["Job Title"].value_counts().index\
-                                              , df["Job Title"].value_counts(),\
-                                              title="Titles Job Posts Count")
+    # script_industries,  div_industries = plot(df["Industry"].value_counts().index\
+    #                                           , df["Industry"].value_counts(),
+    #                                           title="Industry Job Posts Count")
+    # script_companies, div_companies = plot(df["Company Name"].value_counts().index\
+    #                                           , df["Company Name"].value_counts(),\
+    #                                           title="Company Job Posts Count")
+    # script_titles, div_titles = plot(df["Job Title"].value_counts().index\
+    #                                           , df["Job Title"].value_counts(),\
+    #                                           title="Titles Job Posts Count")
     
     
-    kwargs = {'script': script_industries, 'div': div_industries,  'script_titles':script_titles\
-              ,'div_titles': div_titles,\
-             'script_companies':script_companies\
-              ,'div_companies': div_companies}
+    # kwargs = {'script': script_industries, 'div': div_industries,  'script_titles':script_titles\
+    #           ,'div_titles': div_titles,\
+    #           'script_companies':script_companies\
+    #           ,'div_companies': div_companies}
     return render_template('index.html', **kwargs)   
 
 
+
+
 if __name__ == '__main__':
+    # chart()
     app.run(debug=True)
